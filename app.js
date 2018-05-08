19f16f8e249895e4fdcc3e73493aac59c7325e97
tareksalem <tarekpopp@gmail.com>
2018-05-08 13:31:04 +0200

    x

--- app.js
+++ app.js
@@ -3,27 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 const bodyParser = require("body-parser");
-const multer = require("multer");
-var upload = multer({dest: "uploads"});
 const url = require("url");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static(path.join(__dirname, "public"))),
 
-
-// app.use("*", function (req, res) {
-//     // var link = url.parse(req.url);
-//
-// });
 app.use(express.static(path.join(__dirname, "public")));
-console.log(path.join(__dirname, "public"))
 app.use("/*", function (req, res) {
-    // var link = req.headers.host;
-    if (!req.headers.ajax) {
-        res.sendFile(path.join(__dirname, "index.html"));
-    } else {
-        res.status(404).send("error");
-    }
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port);
