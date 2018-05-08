a4747f196586c1e7da0aa657c7d6ca108d56a46c
tareksalem <tarekpopp@gmail.com>
2018-05-08 14:00:41 +0200

    v

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const path = require("path");
 // const bodyParser = require("body-parser");
 const url = require("url");
@@ -8,7 +8,7 @@ const url = require("url");
 // app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/*", function (req, res) {
+app.use("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
